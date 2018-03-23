69d82c84b08244c492c3dcee97c66982c267be89
Omar Ihab Hamed Handouk <o.i.handouk@gmail.com>
2018-03-23 10:24:27 +0200

    [*] Modification to Eight Queens Chess Problem

--- Solutions/University of Valladolid Online Judge/UVa-750-8 Queens Chess Problem (Backtracking).java
+++ Solutions/University of Valladolid Online Judge/UVa-750-8 Queens Chess Problem (Backtracking).java
@@ -5,10 +5,12 @@ import java.io.PrintStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
-import java.util.InputMismatchException;
-import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStream;
 
 /**
@@ -21,149 +23,75 @@ public class Main {
     public static void main(String[] args) {
         InputStream inputStream = System.in;
         OutputStream outputStream = System.out;
-        InputReader in = new InputReader(inputStream);
+        Scanner in = new Scanner(inputStream);
         OutputWriter out = new OutputWriter(outputStream);
-        _8Queens solver = new _8Queens();
+        Eight_Queens solver = new Eight_Queens();
         solver.solve(1, in, out);
         out.close();
     }
 
-    static class _8Queens {
-        int[] queens = new int[8];
-        int r;
-        int cc;
-        int line;
-        int t;
-        boolean p = false;
+    static class Eight_Queens {
+        int lines;
+        int[] rows;
+        int row;
+        int column;
 
-        public void solve(int testNumber, InputReader in, OutputWriter out) {
+        public void solve(int testNumber, Scanner in, OutputWriter out) {
 
-            t = in.nextInt();
+            int q = in.nextInt();
 
-            while (t-- > 0) {
+            while (q-- > 0) {
+
+                row = in.nextInt() - 1;
+                column = in.nextInt() - 1;
+
                 System.out.println("SOLN       COLUMN");
-                System.out.println(" #      1 2 3 4 5 6 7 8");
+                System.out.println(" #      1 2 3 4 5 6 7 8\n");
 
-                line = 1;
-                p = false;
+                rows = new int[8];
+                lines = 0;
 
-                r = in.nextInt() - 1;
-                cc = in.nextInt() - 1;
-                rec(0);
+                backtrack(0);
 
-                System.out.println();
-
-                if (p && t != 0) // Handle the last line, i.e put a new line if there is another test case to create a space
-                    System.out.println();
+                if (q != 0) System.out.println();
             }
         }
 
-        void rec(int c) {
-            if (c == 8 && queens[cc] == r) {
-                print();
-            }
+        void backtrack(int c) {
 
-            for (int i = 0; i < 8; ++i) {
-                if (check(c, i)) {
-                    queens[c] = i;
-                    rec(c + 1);
+            if (c == 8 && rows[column] == row)
+                print();
+
+            for (int r = 0; r < 8; ++r) {
+                if (place(r, c)) {
+                    rows[c] = r;
+                    backtrack(c + 1);
                 }
             }
         }
 
-        boolean check(int c, int r) {
-            for (int i = 0; i < c; ++i) {
-                if (queens[i] == r || Math.abs(i - c) == Math.abs(queens[i] - r))
+        boolean place(int r, int c) {
+            for (int i = 0; i < c; ++i)
+                if (rows[i] == r || Math.abs(rows[i] - r) == Math.abs(i - c))
                     return false;
-            }
-
             return true;
         }
 
         void print() {
-            System.out.println();
-            p = true;
 
-            if (line < 10)
-                System.out.printf(" %d      ", line++);
+            lines += 1;
+
+            if (lines < 10)
+                System.out.print(" " + lines + "      ");
             else
-                System.out.printf("%d      ", line++);
+                System.out.print(lines + "      ");
 
             for (int i = 0; i < 8; ++i) {
-                if (i != 7)
-                    System.out.print(queens[i] + 1 + " ");
+                if (i == 7)
+                    System.out.println(rows[i] + 1);
                 else
-                    System.out.print(queens[i] + 1);
+                    System.out.print(rows[i] + 1 + " ");
             }
-        }
-
-    }
-
-    static class InputReader {
-        private InputStream stream;
-        private byte[] buf = new byte[1024];
-        private int curChar;
-        private int numChars;
-        private InputReader.SpaceCharFilter filter;
-
-        public InputReader(InputStream stream) {
-            this.stream = stream;
-        }
-
-        public int read() {
-            if (numChars == -1) {
-                throw new InputMismatchException();
-            }
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException e) {
-                    throw new InputMismatchException();
-                }
-                if (numChars <= 0) {
-                    return -1;
-                }
-            }
-            return buf[curChar++];
-        }
-
-        public int nextInt() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            int res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        public boolean isSpaceChar(int c) {
-            if (filter != null) {
-                return filter.isSpaceChar(c);
-            }
-            return isWhitespace(c);
-        }
-
-        public static boolean isWhitespace(int c) {
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-        }
-
-        public interface SpaceCharFilter {
-            public boolean isSpaceChar(int ch);
-
         }
 
     }
@@ -181,6 +109,35 @@ public class Main {
 
         public void close() {
             writer.close();
+        }
+
+    }
+
+    static class Scanner {
+        private BufferedReader bufferedReader;
+        private StringTokenizer stringTokenizer;
+
+        public Scanner(InputStream stream) {
+            bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (stringTokenizer == null || !stringTokenizer.hasMoreTokens()) {
+                try {
+                    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                } catch (IOException Error) {
+                    Error.printStackTrace();
+                    ;
+                }
+            }
+
+            return stringTokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            int number = Integer.parseInt(this.next());
+
+            return number;
         }
 
     }
